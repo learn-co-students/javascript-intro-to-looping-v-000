f763fa665fb2e40a19146df484d927557b788e54
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-16 22:43:15 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,33 @@
 function forLoop(array){
   for(var i = 0; i < 25; i++){
     if(i === 1){
-      // console.log(i);
       debugger;
       array.push(`I am ${i} strange loop.`)
     } else {
       array.push(`I am ${i} strange loops.`)
     }
   }
+  return array
+}
+
+function whileLoop(number){
+  while(number >= 0){
+    console.log(number);
+    number--;
+    if(number === 0){
+      // console.log(number);
+      return 'done';
+    }
+  }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do {
+    array.shift();
+  } while (array.length > 0 && maybeTrue());
+  return array;
 }
