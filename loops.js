229d4e38330670b1c302896c6e4d706ea0fce503
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-12 01:25:46 +0000

    Done.

--- loops.js
+++ loops.js
@@ -15,15 +15,16 @@ function whileLoop(num){
     console.log(num);
     num--;
   }
+  return 'done';
 }
 
 function maybeTrue() {
   return Math.random() >= 0.5
-} 
+}
 
 function doWhileLoop(array){
   do {
     array.pop();
-  } while (maybeTrue() && array.length > 0 );  
+  } while (maybeTrue() && array.length > 0 );
   return array;
 }
