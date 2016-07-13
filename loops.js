947a412cb630079f7900dc476853c07bc4912fb6
hudbuz <hzb2@cornell.edu>
2016-07-13 16:21:54 -0400

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
-"use strict";
+
 
 
 
   function forLoop(array) {
-    for (i = 1; i < array.length; i++) {
+    for (var i = 1; i < 26; i++) {
       if (i === 1) {
         array.push("I am 1 stange loop.");
 
@@ -14,3 +14,22 @@
     };
     return array; 
     }
+
+function whileLoop(number) {
+  countdown = number;
+  while (countdown > 0) {
+    console.log(--countdown);
+  };
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array) {
+  do {array.shift();
+  }
+  while (array.length > 0 && maybeTrue())
+  return array;
+}
