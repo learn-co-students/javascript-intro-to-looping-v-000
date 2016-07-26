44feb3128470421172d52660e2186a38d1e4a711
Jason Casares <jasonc@csdrafting.com>
2016-07-26 22:32:47 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i = 1) {
+      return "I am " + i + " strange loop."
+    } else {
+      return "I am " + i + " strange loops."
+    }
+  }
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    n--;
+    if (n == 0) {
+      return "done"
+    }
+  }
+}
