e5ea9c6f775fa569d826929b827ddab9d2b25b8b
reichardn <reichardn@gmail.com>
2016-08-01 12:54:44 -0400

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+
+
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loops.`)
+  }
+  return array
+}
+
+function whileLoop(num) {
+  var countdown = num;
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  return "done"
+}
+
+function doWhileLoop(array) {
+  
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop()
+  } while (maybeTrue() && array.length > 0);
+
+}
