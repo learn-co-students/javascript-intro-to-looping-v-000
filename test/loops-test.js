7e385bbf23ccb4864e06850a8744aab5bd10162f
Megan Eloise <mev277@nyu.edu>
2016-09-20 14:42:12 +0200

    Done.

--- test/loops-test.js
+++ test/loops-test.js
@@ -26,21 +26,6 @@ describe('loops', () => {
     src: fs.readFileSync(path.resolve(__dirname, '..', 'loops.js'), 'utf-8')
   })
 
-  describe('forLoop(array)', () => {
-    it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
-      const [array, t] = makeArray()
-      const strangeArray = forLoop(array)
-
-      expect(strangeArray.length).to.equal(t + 25)
-
-      const testArray = strangeArray.slice(array.length)
-
-      for (let i = 0, l = testArray.length; i < l; i++) {
-        let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
-        expect(testArray[i]).to.equal(s)
-      }
-    })
-  })
 
   describe('whileLoop(n)', () => {
     it('counts down from n to 0', () => {
