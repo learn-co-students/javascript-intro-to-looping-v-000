0c288586321a4802b9432e651734028b45357298
auranbuckles <auran59@gmail.com>
2016-10-09 15:30:58 -0400

    Done.

---
+++ loops.js
@@ -0,0 +1,17 @@
+// function forLoop() {
+// 	for (var i = 0; i < 25; i++) {
+// 		if (i === 1) {
+// 			return "I am " + i + " strange loop."
+// 		} else {
+// 			return "I am " + i + " strange loops."
+// 		};
+// 	};
+// };
+
+// function whileLoop(n) {
+
+// };
+
+// function doWhileLoop(array) {
+
+// };
