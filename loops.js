8b3052176e8e9e79c3f3f842ab0a75280036ff97
shoko furushita <shktkt219@gmail.com>
2016-12-21 10:56:54 -0500

    Done.

--- loops.js
+++ loops.js
@@ -1,19 +1,19 @@
-function forLoop(array){
-  for (let i = 0; i < 25; i++){
-    if (i === 1){
-      array.push("I am 1 strange loop.")
-    } else {
-      array.push(`I am ${i} strange loops.`)
+function forLoop(arr){
+  for(let i=0; i<25; i++){
+    if(i === 1){
+      arr.push("I am 1 strange loop.");
+    }else{
+      arr.push(`I am ${i} strange loops.`);
     }
   }
-  return array
+  return arr;
 }
 
-function whileLoop(num){
-  while(num>0){
-    console.log(--num)
+function whileLoop(number){
+  while(number>0){
+    console.log(--number)
   }
-  return 'done'
+  return "done";
 }
 
 function doWhileLoop(array){
@@ -21,8 +21,8 @@ function doWhileLoop(array){
     return Math.random() >= 0.5
   }
 
-  do {
+  do{
     array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
+  }while(array.length>0 && maybeTrue())
   return array
 }
