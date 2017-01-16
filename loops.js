f8dca2257edb8c2c17fc4719550c661d56e445d8
Joe <jsbenning4@gmail.com>
2017-01-16 18:59:47 -0500

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function maybeTrue() {
+    return Math.random() >= 0.5; // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+
+function forLoop(array) {
+    for (i = 0; i < 25; i++ ) {
+        if (i == 1) {
+          array.push(`I am ${i} strange loop!`);
+        } else {
+          array.push(`I am ${i} strange loops!`);
+        }
+    }
+  return array;
+}
+
+
+function whileLoop(num) {
+    while (num > 0) {
+      console.log(`${num}...`);
+      num--;
+    }
+  return 'done';
+}
+
+function doWhileLoop(array) {
+    do {
+      array.pop()
+    } while ((array.length > 0) && maybeTrue()); 
+    return array;
+}
