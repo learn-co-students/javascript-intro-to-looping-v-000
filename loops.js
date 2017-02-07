2305b11f000de5889a6f11881b69e0f5bfb2a9bf
Daniel Suarez <suarez.daniel101@gmail.com>
2017-02-07 04:28:26 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,7 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++)
+  { if i = 1
+    console.log( "Hello World the " + i + " time" );
+    else console.log( "I am ${i} strange loops.");
+  }
+}
