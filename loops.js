4745ef741df7aea892a87fd74c2d223697e428f7
smithwebtek <brad@smithwebtek.com>
2017-02-22 12:58:36 -0500

    Done.

--- loops.js
+++ loops.js
@@ -1,28 +1,30 @@
 "use strict";
+
 function forLoop(ary) {
     var i = 0;
     for (i = 0; i < 25; i += 1) {
         if (i === 1) {
             ary.push("I am 1 strange loop.");
         } else {
-            ary.push("I am ${i} strange loops.");
+            ary.push(`I am ${i} strange loops.`);
         }
     }
+    debugger;
     return ary;
 }
 
 function whileLoop(num) {
-  let countdown = num;
-  if (countdown !== 0) {
-    while (countdown > 0) {
-    console.log(--countdown);
-    } return "done";
-  } else {
-    return "done";
-  }
+    let countdown = num;
+    if (countdown !== 0) {
+        while (countdown > 0) {
+            console.log(--countdown);
+        }
+        return "done";
+    } else {
+        return "done";
+    }
 }
 
- 
 function doWhileLoop(ary) {
     var asdf = function maybeTrue(blurf) {
         blurf.length === 0;
