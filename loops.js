2d6a663370c0aeee810af0dfd8b1c3c321cd6348
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-06 17:44:51 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,37 +1,29 @@
 function forLoop(array) {
-  
-  
-  for(let i = 0; i < 25; i++) {
+
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {
-    array.push(`I am 1 strange loop.`);
-    }
-  else {
-    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
     }
   }
-  
-  return array;
-};
-
-function whileLoop(number) {
-
-while(number > 0) {
-  console.log(number);
-  number--;
-}
-  return(`done`);
+  return array
 }
 
-function doWhileLoop(array) {
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return "done"
+}
 
 function maybeTrue() {
   return Math.random() >= 0.5
 }
 
-do {
-  array.splice(0, 1);
-} while (maybeTrue() && array.length > 0);
-
-  return array;
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (maybeTrue() && array.length > 0)
+  return array
 }
-  
