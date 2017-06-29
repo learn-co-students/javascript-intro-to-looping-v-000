2c09b798f7b5d365abef6756305c29ea284b3d59
Elias Ventura <eliasventura23@gmail.com>
2017-06-29 18:56:14 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array){
+   for(var i = 0, i < 25, 1++){
+     if(i === 1){
+       return `I am ${i} strange loop.`
+     }
+     else{
+       return `I am ${i} strange loops.`
+     }
+   }
+}
