b5bb08c0ed5589f4b8efdcc50addb48a831d434c
Elias Ventura <eliasventura23@gmail.com>
2017-07-02 02:17:16 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,28 @@
 function forLoop(array){
-   for(var i = 0, i < 25, 1++){
-     if(i === 1){
-       return `I am ${i} strange loop.`
-     }
-     else{
-       return `I am ${i} strange loops.`
-     }
-   }
+  for(var i = 0; i < 25; i++){
+    if(i === 1){
+       array.push(`I am $(i) strange loop.`)
+    }
+    else{
+       array.push(`I am $(i) strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while(n > 0){
+  console.log(--n)}
+return `done`
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  array.pop()
+} while (array.length > 0 && maybeTrue());
+return array
 }
