68c844d6a738c53d364f462b2ffba7f7ba796242
brandon wirz <brandonwirz2@brandons-MacBook-Pro.local>
2017-09-21 15:24:15 -0600

    Done.

--- loops.js
+++ loops.js
@@ -1,17 +1,17 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-    if (i === 1) {
-    array.push("I am 1 strange loop.")
-  }else {
-    array.push(`I am ${i} strange loops.`)
+    if(i === 1){
+      array.push("I am 1 strange loop.")
+    }else{
+      array.push("I am ${i} strange loops.")
     }
   }
-  return array
+return array
 }
 
 function whileLoop(num){
   while(num > 0){
-  console.log(--num)
+    console.log(--num)
   }
   return 'done'
 }
@@ -19,10 +19,11 @@ function whileLoop(num){
 function doWhileLoop(array){
   function maybeTrue() {
     return Math.random() >= 0.5
-  }
+   }
 
 do {
   array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
-  return array
+} while (array.length > 0 && maybeTrue())
+
+ return array
 }
